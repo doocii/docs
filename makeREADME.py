cea36329d2461f44ae06da1498ed947fb01cca4f
metaldudu <metaldudu@gmail.com>
2019-02-02 14:59:54 +0000

    “updatedocs”

--- makeREADME.py
+++ makeREADME.py
@@ -4,21 +4,25 @@
 
 import os
 import sys
+import datetime
 
 f = open('README.md','w',encoding='utf-8')
+f.write('# docs\n') # 页面H1
 files = os.listdir(os.getcwd())
 files.sort()
 
 for i in files:
     fpath = os.path.join(os.getcwd(),i)
     if (i[0] == '.'):
-        pass#排除隐藏目录
+        pass #排除隐藏目录
     elif os.path.isdir(fpath):
         f.write('\n\n## ' + i + '\n\n')#文件夹
         for j in os.listdir(fpath):
-            if j != 'README.md' and j != 'makeREADME.py':
-                link = '- [' + j.replace('.md', '') + '](' + j + ')\n'
-            f.write(link)
+            if j != 'README.md':
+                if j != 'makeREADME.py':
+                    link = '- [' + j.replace('.md', '') + '](' + i + '/' + j + ')\n'
+                    f.write(link)
 print ('done!')
 
+f.write('\n--- \n\nUPDATE: ' + str(datetime.date.today()))
 f.close()
